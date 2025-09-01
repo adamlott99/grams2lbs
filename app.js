2dfc5ecf0b43285ca7428d67f232fa80ca66b03e
Adam Lott <adamlott99@gmail.com>
2025-08-31 21:44:07 -0500

    add conversion back to grams

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-import { gramsToPoundsOunces } from './logic.js';
+import { gramsToPoundsOunces, poundsOuncesToGrams, PoundsOunces } from './logic.js';
+
+function updatePoundsOuncesOutput() {
+    const poundsOunces = gramsToPoundsOunces(gramsInput.value);
+    poundsOuncesOutput.textContent = `${poundsOunces.pounds} lbs ${poundsOunces.ounces.toFixed(1)} oz`;
+}
+
+function updateGramsOutput() {
+    const poundsOunces = new PoundsOunces(poundsInput.value, ouncesInput.value);
+    const grams = poundsOuncesToGrams(poundsOunces);
+    gramsOutput.textContent = `${grams.toFixed(1)} grams`;
+}
 
 const gramsInput = document.getElementById('gramsInput');
 const poundsOuncesOutput = document.getElementById('poundsOuncesOutput');
-gramsInput.addEventListener("input", () => {
-    const poundsOunces = gramsToPoundsOunces(gramsInput.value);
-    poundsOuncesOutput.textContent = `${poundsOunces.pounds} lbs ${poundsOunces.ounces.toFixed(1)} oz`;
-});
+gramsInput.addEventListener("input", updatePoundsOuncesOutput);
+
+const poundsInput = document.getElementById('poundsInput');
+const ouncesInput = document.getElementById('ouncesInput');
+const gramsOutput = document.getElementById('gramsOutput');
+poundsInput.addEventListener("input", updateGramsOutput);
+ouncesInput.addEventListener("input", updateGramsOutput);
--- logic.js
+++ logic.js
@@ -2,7 +2,7 @@ const GRAMS_PER_POUND = 453.592;
 const OUNCES_PER_POUND = 16;
 const GRAMS_PER_OUNCE = GRAMS_PER_POUND / OUNCES_PER_POUND;
 
-class PoundsOunces {
+export class PoundsOunces {
     constructor(pounds, ounces) {
         this.pounds = pounds;
         this.ounces = ounces;
@@ -13,4 +13,9 @@ export function gramsToPoundsOunces(grams) {
     const pounds = Math.floor(grams / GRAMS_PER_POUND);
     const ounces = (grams % GRAMS_PER_POUND) / GRAMS_PER_OUNCE;
     return new PoundsOunces(pounds, ounces);
+}
+
+export function poundsOuncesToGrams(poundsOunces) {
+    const grams = poundsOunces.pounds * GRAMS_PER_POUND + poundsOunces.ounces * GRAMS_PER_OUNCE;
+    return grams;
 }
